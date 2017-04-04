ed9e024511ffab2909ebdf19439212a169cbe923
Andrei Negruti <Andrei.Negruti@PaddyPowerBetfair.com>
2017-04-04 12:23:58 +0300

    Mapping now works

---
+++ src/main/java/application/repository/BetRepository.java
@@ -0,0 +1,8 @@
+package application.repository;
+
+import application.domain.Bet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BetRepository extends JpaRepository<Bet, Integer> {}
